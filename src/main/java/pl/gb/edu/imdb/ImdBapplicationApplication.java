b9ae2d679db777fb560cd4d0f4659a17eea5c309
Grzegorz Banaszek <grzegorz.banaszek@gmail.com>
2019-10-27 12:39:15 +0100

    initial commit

---
+++ src/main/java/pl/gb/edu/imdb/ImdBapplicationApplication.java
@@ -0,0 +1,13 @@
+package pl.gb.edu.imdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImdBapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImdBapplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/gb/edu/imdb/ImdBapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.gb.edu.imdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImdBapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
