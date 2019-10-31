a111bb9c904b32f787fd4bd429790c15d744a664
Grzegorz Banaszek <grzegorz.banaszek@gmail.com>
2019-10-31 09:49:03 +0100

    Added context path

--- src/main/java/pl/gb/edu/imdb/controller/rest/ActorRestController.java
+++ src/main/java/pl/gb/edu/imdb/controller/rest/ActorRestController.java
@@ -1,18 +1,24 @@
 package pl.gb.edu.imdb.controller.rest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import pl.gb.edu.imdb.model.Actor;
 import pl.gb.edu.imdb.model.Role;
 import pl.gb.edu.imdb.service.ActorService;
 import pl.gb.edu.imdb.service.RoleService;
 
+import java.security.Principal;
 import java.util.List;
 
-@RestController("/v1")
+@RestController
 @RequestMapping(value = "/actors")
 public class ActorRestController {
     private ActorService actorService;
@@ -30,12 +36,12 @@ public class ActorRestController {
     }
 
     @GetMapping("/{id}")
-    public Actor getActorById(@PathVariable("id") Long id) {
-        return actorService.getActorById(id);
+    public ResponseEntity<Actor> getActorById(@PathVariable("id") Long id) {
+        return new ResponseEntity<>(actorService.getActorById(id), HttpStatus.OK);
     }
 
     @GetMapping("/{id}/appearances")
-    public List<Role> showActorRoles(@PathVariable("id") Long id) {
-        return roleService.getRolesByActorId(id);
+    public ResponseEntity<List<Role>> showActorRoles(@PathVariable("id") Long id) {
+        return new ResponseEntity<>(roleService.getRolesByActorId(id), HttpStatus.OK);
     }
 }
